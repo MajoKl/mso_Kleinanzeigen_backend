e6cd2e5f043c44153bd8fe389b0a2067f975b249
21Jon <l.jonas.1@web.de>
2022-02-22 16:23:13 +0100

    neue dev dependenzies

---
+++ tests/test/tess.test.js
@@ -0,0 +1,8 @@
+describe(" fuck this shit no one swars like that ", () => {
+  test("at least this shit works", () => {
+    expect(2).not.toBe(1);
+  });
+
+  it.todo("fuck pita");
+  test.todo("fuck peta");
+});
